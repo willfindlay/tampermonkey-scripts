e28da6cb5f50956f6e2281fbf142d28684c9741c
William Findlay <william@williamfindlay.com>
2020-10-23 11:30:38 -0400

    Clean up

--- scripts/carleton-proxy/acm-carleton.js
+++ scripts/carleton-proxy/acm-carleton.js
@@ -2,15 +2,16 @@
 // @name        Carleton ACM Redirect
 // @version     0.1
 // @description This redirects an ACM DL page to the Carleton Proxy
-// @match       https://dl.acm.org/doi/*
+// @author      William Findlay
 // @copyright   2020, William Findlay
+// @match       https://dl.acm.org/doi/*
 // @run-at      document-start
 // ==/UserScript==
 
-reToPage("https://dl.acm.org/doi/");
+redirect("https://dl.acm.org/doi/");
 
-function reToPage(currentpage){
-    if(window.location.href.indexOf(currentpage) != -1){
+function redirect(page){
+    if(window.location.href.indexOf(page) != -1){
         window.location.href = window.location.href.replace("https://dl.acm.org/", "https://dl-acm-org.proxy.library.carleton.ca/");
     }
 }
--- scripts/carleton-proxy/ieee-carleton.js
+++ scripts/carleton-proxy/ieee-carleton.js
@@ -2,17 +2,18 @@
 // @name        Carleton IEEE Redirect
 // @version     0.1
 // @description This redirects an IEEE Xplore page to the Carleton Proxy
+// @author      William Findlay
+// @copyright   2020, William Findlay
 // @match       https://ieeexplore.ieee.org/abstract/document/*
 // @match       https://ieeexplore.ieee.org/document/*
-// @copyright   2020, William Findlay
 // @run-at      document-start
 // ==/UserScript==
 
-reToPage("https://ieeexplore.ieee.org/document/");
-reToPage("https://ieeexplore.ieee.org/abstract/document/");
+redirect("https://ieeexplore.ieee.org/document/");
+redirect("https://ieeexplore.ieee.org/abstract/document/");
 
-function reToPage(currentpage){
-    if(window.location.href.indexOf(currentpage) != -1){
+function redirect(page){
+    if(window.location.href.indexOf(page) != -1){
         window.location.href = window.location.href.replace("https://ieeexplore.ieee.org/", "https://ieeexplore-ieee-org.proxy.library.carleton.ca/");
     }
 }
